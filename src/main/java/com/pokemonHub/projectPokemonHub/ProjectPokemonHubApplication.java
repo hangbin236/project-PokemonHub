3474eaa571464039e6b7e8b93c6c61f51272c3f8
hangbin236 <hangbin236@revature.net>
2022-11-11 21:02:52 -0500

    Initial commit

---
+++ src/main/java/com/pokemonHub/projectPokemonHub/ProjectPokemonHubApplication.java
@@ -0,0 +1,13 @@
+package com.pokemonHub.projectPokemonHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPokemonHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPokemonHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokemonHub/projectPokemonHub/ProjectPokemonHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemonHub.projectPokemonHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPokemonHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
