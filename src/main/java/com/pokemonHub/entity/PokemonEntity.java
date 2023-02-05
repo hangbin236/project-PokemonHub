4c9df6dd1cfc447299377922094ae07f5b989eaf
hangbin236 <hangbin236@revature.net>
2023-02-05 18:45:46 -0500

    used lombok data

--- src/main/java/com/pokemonHub/entity/PokemonEntity.java
+++ src/main/java/com/pokemonHub/entity/PokemonEntity.java
@@ -1,7 +1,5 @@
 package com.pokemonHub.entity;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name="pokemon_details")
+@Data
 public class PokemonEntity {
 
 		@Id
@@ -33,126 +34,9 @@ public class PokemonEntity {
 
 		@Column(name ="pokemon_image")
 		private String pokemonImage;
-
-		/**
-		 * 
-		 */
+		
 		public PokemonEntity() {
-		
-		}
-
-		/**
-		 * @param pokemonId
-		 * @param pokemonName
-		 * @param pokemonSetName
-		 * @param pokemonCondition
-		 * @param pokemonPrice
-		 * @param pokemonImage
-		 */
-		public PokemonEntity(int pokemonId, String pokemonName, String pokemonSetName, String pokemonCondition,
-				int pokemonPrice, String pokemonImage) {
 			super();
-			this.pokemonId = pokemonId;
-			this.pokemonName = pokemonName;
-			this.pokemonSetName = pokemonSetName;
-			this.pokemonCondition = pokemonCondition;
-			this.pokemonPrice = pokemonPrice;
-			this.pokemonImage = pokemonImage;
 		}
-		
-		
-
-		/**
-		 * @param pokemonName
-		 * @param pokemonSetName
-		 * @param pokemonCondition
-		 * @param pokemonPrice
-		 * @param pokemonImage
-		 */
-		public PokemonEntity(String pokemonName, String pokemonSetName, String pokemonCondition, int pokemonPrice,
-				String pokemonImage) {
-			super();
-			this.pokemonName = pokemonName;
-			this.pokemonSetName = pokemonSetName;
-			this.pokemonCondition = pokemonCondition;
-			this.pokemonPrice = pokemonPrice;
-			this.pokemonImage = pokemonImage;
-		}
-
-		public int getPokemonId() {
-			return pokemonId;
-		}
-
-		public void setPokemonId(int pokemonId) {
-			this.pokemonId = pokemonId;
-		}
-
-		public String getPokemonName() {
-			return pokemonName;
-		}
-
-		public void setPokemonName(String pokemonName) {
-			this.pokemonName = pokemonName;
-		}
-
-		public String getPokemonSetName() {
-			return pokemonSetName;
-		}
-
-		public void setPokemonSetName(String pokemonSetName) {
-			this.pokemonSetName = pokemonSetName;
-		}
-
-		public String getPokemonCondition() {
-			return pokemonCondition;
-		}
-
-		public void setPokemonCondition(String pokemonCondition) {
-			this.pokemonCondition = pokemonCondition;
-		}
-
-		public int getPokemonPrice() {
-			return pokemonPrice;
-		}
-
-		public void setPokemonPrice(int pokemonPrice) {
-			this.pokemonPrice = pokemonPrice;
-		}
-
-		public String getPokemonImage() {
-			return pokemonImage;
-		}
-
-		public void setPokemonImage(String pokemonImage) {
-			this.pokemonImage = pokemonImage;
-		}
-
-		@Override
-		public String toString() {
-			return "PokemonEntity [pokemonId=" + pokemonId + ", pokemonName=" + pokemonName + ", pokemonSetName="
-					+ pokemonSetName + ", pokemonCondition=" + pokemonCondition + ", pokemonPrice=" + pokemonPrice
-					+ ", pokemonImage=" + pokemonImage + "]";
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(pokemonCondition, pokemonId, pokemonImage, pokemonName, pokemonPrice, pokemonSetName);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			PokemonEntity other = (PokemonEntity) obj;
-			return Objects.equals(pokemonCondition, other.pokemonCondition) && pokemonId == other.pokemonId
-					&& Objects.equals(pokemonImage, other.pokemonImage)
-					&& Objects.equals(pokemonName, other.pokemonName) && pokemonPrice == other.pokemonPrice
-					&& Objects.equals(pokemonSetName, other.pokemonSetName);
-		}
-		
-		
+	
 }
--- src/main/java/com/pokemonHub/pojo/PokemonPojo.java
+++ src/main/java/com/pokemonHub/pojo/PokemonPojo.java
@@ -1,20 +1,11 @@
 package com.pokemonHub.pojo;
 
-import java.util.Objects;
-
-import org.jetbrains.annotations.NotNull;
-
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 @NoArgsConstructor
-@Getter
-@Setter
-@ToString
 @EqualsAndHashCode
+@Data
 public class PokemonPojo {
 
 	
@@ -32,13 +23,6 @@ public class PokemonPojo {
 	
 	private String pokemonImage;
 
-	/**
-	 * 
-	 */
-	public PokemonPojo() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
 	public PokemonPojo(int pokemonId, String pokemonName, String pokemonSetName, String pokemonCondition,
 			int pokemonPrice, String pokemonImage) {
@@ -50,97 +34,7 @@ public class PokemonPojo {
 		this.pokemonPrice = pokemonPrice;
 		this.pokemonImage = pokemonImage;
 	}
-
-	/**
-	 * @param pokemonName
-	 * @param pokemonSetName
-	 * @param pokemonCondition
-	 * @param pokemonPrice
-	 * @param pokemonImage
-	 */
-	public PokemonPojo(String pokemonName, String pokemonSetName, String pokemonCondition, int pokemonPrice,
-			String pokemonImage) {
-		super();
-		this.pokemonName = pokemonName;
-		this.pokemonSetName = pokemonSetName;
-		this.pokemonCondition = pokemonCondition;
-		this.pokemonPrice = pokemonPrice;
-		this.pokemonImage = pokemonImage;
-	}
-
-	public int getPokemonId() {
-		return pokemonId;
-	}
-
-	public void setPokemonId(int pokemonId) {
-		this.pokemonId = pokemonId;
-	}
-
-	public String getPokemonName() {
-		return pokemonName;
-	}
-
-	public void setPokemonName(String pokemonName) {
-		this.pokemonName = pokemonName;
-	}
-
-	public String getPokemonSetName() {
-		return pokemonSetName;
-	}
-
-	public void setPokemonSetName(String pokemonSetName) {
-		this.pokemonSetName = pokemonSetName;
-	}
-
-	public String getPokemonCondition() {
-		return pokemonCondition;
-	}
-
-	public void setPokemonCondition(String pokemonCondition) {
-		this.pokemonCondition = pokemonCondition;
-	}
-
-	public int getPokemonPrice() {
-		return pokemonPrice;
-	}
-
-	public void setPokemonPrice(int pokemonPrice) {
-		this.pokemonPrice = pokemonPrice;
-	}
-
-	public String getPokemonImage() {
-		return pokemonImage;
-	}
-
-	public void setPokemonImage(String pokemonImage) {
-		this.pokemonImage = pokemonImage;
-	}
-
-	@Override
-	public String toString() {
-		return "PokemonPojo [pokemonId=" + pokemonId + ", pokemonName=" + pokemonName + ", pokemonSetName="
-				+ pokemonSetName + ", pokemonCondition=" + pokemonCondition + ", pokemonPrice=" + pokemonPrice
-				+ ", pokemonImage=" + pokemonImage + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(pokemonCondition, pokemonId, pokemonImage, pokemonName, pokemonPrice, pokemonSetName);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PokemonPojo other = (PokemonPojo) obj;
-		return Objects.equals(pokemonCondition, other.pokemonCondition) && pokemonId == other.pokemonId
-				&& Objects.equals(pokemonImage, other.pokemonImage) && Objects.equals(pokemonName, other.pokemonName)
-				&& pokemonPrice == other.pokemonPrice && Objects.equals(pokemonSetName, other.pokemonSetName);
-	}
-
+	
+	
 
 }
